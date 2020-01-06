ea388a4263a9a7ce0d7aa73cfe78e81f881547ef
Gia Do <gia.dohoang@gmail.com>
2020-01-06 14:23:06 -0500

    Test Java Servlet on register page

--- src/java/controller/ProcessRegister.java
+++ src/java/controller/ProcessRegister.java
@@ -30,6 +30,9 @@ public class ProcessRegister extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        String firstName = request.getParameter("first-name");
+        String lastName = request.getParameter("last-name");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
@@ -38,7 +41,8 @@ public class ProcessRegister extends HttpServlet {
             out.println("<title>Servlet ProcessRegister</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ProcessRegister at " + request.getContextPath() + "</h1>");
+            out.println("<h1>First Name: " + firstName + "</h1>");
+            out.println("<h1>Last Name: " + lastName + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
